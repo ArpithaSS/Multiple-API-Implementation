51787ff7b390346a8d4403e7703133e08a29241c
Arpitha <arpithanaik17@gmail.com>
2024-05-19 16:35:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [quote,setQuote]=useState("");
+  const [user, setUser]=useState("");
+  useEffect(()=>{
+    async function fetchQuote() {
+      try{
+      const response= await axios.get('https://programming-quotesapi.vercel.app/api/random');
+      const userResponse= await axios.get('https://randomuser.me/api/');
+      setQuote(response.data.quote);
+      setUser(userResponse.data.results);
+      console.log("Fired once");
+      }
+      catch(error) {
+        console.error("Error fetching quote",quote);
+      }
+    }
+    fetchQuote();
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>Programming Quotes</div>
+    <div>{quote}</div>
+    <div>
+    {user && user.map((item,key)=>{
+      return(
+        <table>
+          <thead>{key}</thead>
+          <tbody>
+            <tr>
+        <div key={key}>{item.login.username}</div>
+        </tr>
+        <tr>
+        {item.email}
+        </tr>
+        </tbody>
+        </table>
+      )
+    })}
+      </div>
+    </>
   );
 }
 
